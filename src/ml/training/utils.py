9b3bad2ec1657385df99a1e27344d7b2df43b421
Han Chong <chuahanchong93@gmail.com>
2021-08-22 22:48:40 +0800

    Format utils

--- src/ml/training/utils.py
+++ src/ml/training/utils.py
@@ -1,24 +1,24 @@
 """Module for training utils."""
+from typing import Any
 from typing import Dict
 
-from tensorflow.keras import losses as losses_mod
-from tensorflow.keras import metrics as metrics_mod
+import tensorflow as tf
 from typeguard import typechecked
 
 
 @typechecked
-def call_loss_object(config: Dict[str, str]):
+def call_loss_object(config: Dict[str, Any]) -> tf.keras.losses:
     """Call a tf.keras loss."""
     if config.get('kwargs'):
-        return eval(f'{losses_mod}.' + config['name'])(**config['kwargs'])
+        return eval('tf.keras.losses.' + config['name'])(**config['kwargs'])
     else:
-        return eval(f'{losses_mod}.' + config['name'])()
+        return eval('tf.keras.losses.' + config['name'])()
 
 
 @typechecked
-def call_metric_object(config):
+def call_metric_object(config: Dict[str, Any]) -> tf.keras.losses:
     """Call a tf.keras metric."""
     if config.get('kwargs'):
-        return eval(f'{metrics_mod}.' + config['name'])(**config['kwargs'])
+        return eval('tf.keras.metrics.' + config['name'])(**config['kwargs'])
     else:
-        return eval(f'{metrics_mod}.' + config['name'])()
+        return eval('tf.keras.metrics.' + config['name'])()
